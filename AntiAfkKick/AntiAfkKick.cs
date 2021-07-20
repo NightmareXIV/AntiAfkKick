99c09bce1995c34a08a752c8d6a0c485784fcd7f
Eternita <lysithea.star@gmail.com>
2021-07-20 10:01:55 +0300

    Add project files.

---
+++ AntiAfkKick/AntiAfkKick.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntiAfkKick
+{
+    class AntiAfkKick
+    {
+        static int NextKeyPress = 0;
+        static NotifyIcon n = null;
+        private static string appGuid = "92f42221-51a5-4753-9e91-84aeea157d17";
+
+        static void Main(string[] args)
+        {
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    MessageBox.Show("Application already running");
+                    return;
+                }
+                n = new NotifyIcon
+                {
+                    Icon = SystemIcons.Application,
+                    Visible = true,
+                    ContextMenu = new ContextMenu(new MenuItem[] { new MenuItem("Exit", delegate { n.Dispose(); Environment.Exit(0); }) })
+                };
+                new Thread((ThreadStart)delegate
+                {
+                   while (true)
+                   {
+                       var proc = Process.GetProcessesByName("ffxiv_dx11");
+                       if (proc.Length == 0)
+                       {
+                           proc = Process.GetProcessesByName("ffxiv");
+                       }
+                       if (proc.Length == 0)
+                       {
+                           Console.WriteLine("Could not find FFXIV window");
+                       }
+                       else
+                       {
+                           if (Environment.TickCount > NextKeyPress &&
+                               (Native.GetForegroundWindow() != proc[0].MainWindowHandle || Native.IdleTimeFinder.GetIdleTime() > 60 * 1000))
+                           {
+                               Console.WriteLine("Sending keypress to FFXIV window");
+                               Native.Keypress.SendKeycode(proc[0].MainWindowHandle, Native.Keypress.LControlKey);
+                               NextKeyPress = Environment.TickCount + new Random().Next(2 * 60 * 1000, 4 * 60 * 1000);
+                           }
+                       }
+                       Thread.Sleep(1000);
+                   }
+               }).Start();
+                Application.Run();
+            }
+        }
+    }
+}
---
+++ AntiAfkKick/Native.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiAfkKick
+{
+    class Native
+    {
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+
+            public uint dwTime;
+        }
+
+        /// <summary>
+        /// Helps to find the idle time, (in milliseconds) spent since the last user input
+        /// </summary>
+        public class IdleTimeFinder
+        {
+            [DllImport("User32.dll")]
+            private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+            [DllImport("Kernel32.dll")]
+            private static extern uint GetLastError();
+
+            public static uint GetIdleTime()
+            {
+                LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+                lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+                GetLastInputInfo(ref lastInPut);
+
+                return ((uint)Environment.TickCount - lastInPut.dwTime);
+            }
+            /// <summary>
+            /// Get the Last input time in milliseconds
+            /// </summary>
+            /// <returns></returns>
+            public static long GetLastInputTime()
+            {
+                LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+                lastInPut.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInPut);
+                if (!GetLastInputInfo(ref lastInPut))
+                {
+                    throw new Exception(GetLastError().ToString());
+                }
+                return lastInPut.dwTime;
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        public class Keypress
+        {
+            public const int LControlKey = 162;
+            public const int Space = 32;
+
+            const uint WM_KEYUP = 0x101;
+            const uint WM_KEYDOWN = 0x100;
+
+            [DllImport("user32.dll")]
+            private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+            public static void SendKeycode(IntPtr hwnd, int keycode)
+            {
+                SendMessage(hwnd, WM_KEYDOWN, (IntPtr)keycode, (IntPtr)0);
+                SendMessage(hwnd, WM_KEYUP, (IntPtr)keycode, (IntPtr)0);
+            }
+        }
+    }
+}
